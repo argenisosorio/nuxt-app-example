9ac285e8f5265cf5a790d1c41920ff1a275e61d6
Argenis Osorio <argenisosorio580@hotmail.com>
2021-08-27 00:32:33 -0400

    Added basic example of Vue in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,16 @@
 <template>
-  <Tutorial />
+  <div>
+    <button class="button" @click="count++">
+      You clicked me {{ count }} times.
+    </button>
+  </div>
 </template>
-
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      count: 0
+    }
+  }
+}
 </script>
