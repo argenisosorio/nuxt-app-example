cfe3d5d0cf7e6093047fb015b6d389d884ba8a43
Argenis Osorio <argenisosorio580@hotmail.com>
2021-09-07 17:01:40 -0400

    Added example of get data witch axios and show data in template

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,28 +1,43 @@
 <template>
   <div>
     <h1>Blog</h1>
-    <ul>
-      <li>
-        <NuxtLink to="/blog/post1">
-          Post 1
-        </NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/blog/post2">
-          Post 2
-        </NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/blog/post3">
-          Post 3
-        </NuxtLink>
-      </li>
-    </ul>
+    <table class="table" border="1px">
+      <thead>
+        <tr>
+          <th scope="col">
+            ID
+          </th>
+          <th scope="col">
+            TITLE
+          </th>
+          <th scope="col">
+            BODY
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(post, index) in posts" :key="index">
+          <td v-text="post.id" />
+          <td v-text="post.title" />
+          <td v-text="post.body" />
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  layout: 'navbar'
+  layout: 'navbar',
+  data () {
+    return {
+      posts: []
+    }
+  },
+  async created () {
+    const resp = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    this.posts = resp.data
+  }
 }
 </script>
