50f5b9d0b8c0a7e33e27f9b4f182b13b768a8bdf
Argenis Osorio <argenisosorio580@hotmail.com>
2021-09-16 20:11:56 -0400

    Added assets and images for tests

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,10 +1,26 @@
 <template>
   <div>
-    <h1>Blog</h1>
+    <main class="container">
+      <NuxtLink to="/">
+        Home
+      </NuxtLink>
+      |
+      <NuxtLink to="/blog">
+        Blog
+      </NuxtLink>
+      <h1
+        class="mt-3"
+      >
+        Blog
+      </h1>
+      <p class="mt-4">
+        Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.
+      </p>
+      <div>
+        <img src="~/assets/img/logo-firefox-quantum.png">
+      </div>
+    </main>
   </div>
 </template>
 <script>
-export default {
-  layout: 'navbar'
-}
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,25 @@
 <template>
   <div>
-
     <main class="container">
-      <div class="p-4 p-md-5 mt-4 mb-4 text-white rounded bg-dark">
-        <div class="col-md-6 px-0">
-          <h1 class="display-4 fst-italic">Title of a longer featured blog post</h1>
-          <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
-          <p class="lead mb-0"><a href="#" class="text-white fw-bold">Continue reading...</a></p>
-        </div>
+      <NuxtLink to="/">
+        Home
+      </NuxtLink>
+      |
+      <NuxtLink to="/blog">
+        Blog
+      </NuxtLink>
+      <h1
+        class="mt-3"
+      >
+        Home
+      </h1>
+      <p class="mt-4">
+        Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.
+      </p>
+      <div>
+        <img src="~/assets/img/Debian_logo.png">
       </div>
-      <article class="blog-post">
-        <h2 class="blog-post-title">Sample blog post</h2>
-        <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
-        <p>This blog post shows a few different types of content that’s supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
-        <p>Yeah, she dances to her own beat. Oh, no. You could've been the greatest. 'Cause, baby, <a href="#">you're a firework</a>. Maybe a reason why all the doors are closed. Open up your heart and just let it begin. So très chic, yeah, she's a classic.</p>
-        <blockquote>
-          <p>Bikinis, zucchinis, Martinis, no weenies. I know there will be sacrifice but that's the price. <strong>This is how we do it</strong>. I'm not sticking around to watch you go down. You think you're so rock and roll, but you're really just a joke. I know one spark will shock the world, yeah yeah. Can't replace you with a million rings.</p>
-        </blockquote>
-        <p>Trying to connect the dots, don't know what to tell my boss. Before you met me I was alright but things were kinda heavy. You just gotta ignite the light and let it shine. Glitter all over the room <em>pink flamingos</em> in the pool. </p>
-        <ul>
-          <li>Got a motel and built a fort out of sheets.</li>
-          <li>Your kiss is cosmic, every move is magic.</li>
-          <li>Suiting up for my crowning battle.</li>
-        </ul>
-        <p>Takes you miles high, so high, 'cause she’s got that one international smile.</p>
-      </article>
-    </main><!-- /.container -->
-
+    </main>
   </div>
 </template>
 <script>
